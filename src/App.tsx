2223e441ed44b4d6738c214b281479d59ef87523
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-04 23:59:44 -0300

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+const swalit = () => {
+  MySwal.fire({
+    title: <p>Hello World</p>,
+    footer: "Copyright 2018",
+    didOpen: () => {
+      // `MySwal` is a subclass of `Swal`
+      //   with all the same instance & static methods
+      MySwal.clickConfirm();
+    },
+  }).then(() => {
+    return MySwal.fire(<p>Shorthand works too</p>);
+  });
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Using SweetAlert2 with React</h1>
+        <button onClick={swalit}>Click me</button>
       </header>
     </div>
   );
